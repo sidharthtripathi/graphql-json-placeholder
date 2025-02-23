af92802f261b5afd0f10b8d69cc165615d631c6e
Sidharth Tripathi <sidharthtripathi807@gmail.com>
2025-02-23 14:41:47 +0530

    added health check status enpoint

--- index.ts
+++ index.ts
@@ -1,12 +1,31 @@
+import express from 'express';
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import { typeDefs,allResolvers } from './graphql/query';
+import { expressMiddleware } from '@apollo/server/express4';
+import { typeDefs, allResolvers } from './graphql/query';
+import bodyParser from 'body-parser';
 
-const server = new ApolloServer({
+async function startServer() {
+  const app = express();
+  app.use(bodyParser.json())
+  // Create Apollo Server instance
+  const server = new ApolloServer({
     typeDefs,
-    resolvers:allResolvers,
-})
+    resolvers: allResolvers,
+  });
+  await server.start();
 
-const {url} = await startStandaloneServer(server,{listen:{port : 3000}})
+  // Attach the Apollo middleware under the /graphql path
+  app.use('/graphql', expressMiddleware(server));
 
-console.log(`server is up at: ${url}`)
+  // Add your custom status endpoint
+  app.get('/status', (req, res) => {
+    res.status(200).json({ status: 'ok' });
+  });
+
+  // Start the Express server
+  app.listen(3000, () => {
+    console.log('Server is up at http://localhost:3000');
+  });
+}
+
+startServer();
